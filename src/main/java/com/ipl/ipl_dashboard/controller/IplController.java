7e0f56ea34c50b6c79a40271a2aab50757e2c6c9
bapan25 <ruptam25@outlook.com>
2021-08-14 11:31:46 +0530

    [added cross origin]

--- src/main/java/com/ipl/ipl_dashboard/controller/IplController.java
+++ src/main/java/com/ipl/ipl_dashboard/controller/IplController.java
@@ -10,12 +10,14 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class IplController {
 
     @Autowired
